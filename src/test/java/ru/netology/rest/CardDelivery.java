a70a05b572458fe86c55eb4d2fb0ed905a4e17fe
Nikolai Pustovoit <nikolaikop@gmail.com>
2021-12-23 22:08:42 -0700

    more tests

--- src/test/java/ru/netology/rest/CardDelivery.java
+++ src/test/java/ru/netology/rest/CardDelivery.java
@@ -2,17 +2,9 @@ package ru.netology.rest;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -33,7 +25,6 @@ public class CardDelivery {
 
     @Test
     void sendIfDataSuccessful (){
-
         $("[placeholder=Город]").setValue("Санкт-Петербург");
         String inputDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         SelenideElement data = $("[data-test-id=date]");
@@ -49,7 +40,89 @@ public class CardDelivery {
                 .shouldHave(Condition.text("Встреча успешно забронирована на " + inputDate));
     }
 
+    @Test
+    void shouldntSendWithoutCity() {
+        String inputDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        SelenideElement data = $("[data-test-id=date]");
+        data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
+        data.$("[placeholder]").setValue(inputDate);
+        $("[data-test-id=name].input_type_text .input__control").setValue("Николай Николаевич");
+        $("[data-test-id=phone]").$("[name=phone]").setValue("+71234567890");
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[data-test-id='city'].input_invalid .input__sub")
+                .shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
 
+    @Test
+    void shouldntSendWithForeignSymbols() {
+        $("[placeholder=Город]").setValue("Saint-Petersburg");
+        String inputDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        SelenideElement data = $("[data-test-id=date]");
+        data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
+        data.$("[placeholder]").setValue(inputDate);
+        $("[data-test-id=name].input_type_text .input__control").setValue("Николай Николаевич");
+        $("[data-test-id=phone]").$("[name=phone]").setValue("+71234567890");
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[data-test-id='city'].input_invalid .input__sub")
+                .shouldHave(Condition.text("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void shouldntSendWithoutName() {
+        $("[placeholder=Город]").setValue("Санкт-Петербург");
+        String inputDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        SelenideElement data = $("[data-test-id=date]");
+        data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
+        data.$("[placeholder]").setValue(inputDate);
+        $("[data-test-id=phone]").$("[name=phone]").setValue("+71234567890");
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[data-test-id='name'].input_invalid .input__sub")
+                .shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldntSendWithoutPhone (){
+        $("[placeholder=Город]").setValue("Санкт-Петербург");
+        String inputDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        SelenideElement data = $("[data-test-id=date]");
+        data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
+        data.$("[placeholder]").setValue(inputDate);
+        $("[data-test-id=name].input_type_text .input__control").setValue("Николай Николаевич");
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[data-test-id='phone'].input_invalid .input__sub")
+                .shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldntSendWithoutCheckBox (){
+        $("[placeholder=Город]").setValue("Санкт-Петербург");
+        String inputDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        SelenideElement data = $("[data-test-id=date]");
+        data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
+        data.$("[placeholder]").setValue(inputDate);
+        $("[data-test-id=name].input_type_text .input__control").setValue("Николай Николаевич");
+        $("[data-test-id=phone]").$("[name=phone]").setValue("+71234567890");
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[data-test-id='agreement'] .checkbox__text")
+                .shouldHave(Condition.text("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+    }
+
+    @Test
+    void shouldntSendWithoutDate() {
+        $("[placeholder=Город]").setValue("Санкт-Петербург");
+        SelenideElement data = $("[data-test-id=date]");
+        data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=name].input_type_text .input__control").setValue("Николай Николаевич");
+        $("[data-test-id=phone]").$("[name=phone]").setValue("+71234567890");
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[data-test-id=date] .input__sub").shouldHave
+                (exactTextCaseSensitive("Неверно введена дата"));
+    }
 
     @Test
     void shouldSubmitTask2() {
@@ -64,7 +137,7 @@ public class CardDelivery {
         $("[data-test-id='notification'] .notification__title").shouldHave(Condition.text("Успешно!"),
                 Duration.ofSeconds(delay));
         $("[data-test-id='notification'] .notification__content")
-                .shouldHave(Condition.text("Встреча успешно забронирована на " + add7Days));
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + LocalDate.now().plusDays(add7Days).format(DateTimeFormatter.ofPattern("d"))));
     }
 
 }

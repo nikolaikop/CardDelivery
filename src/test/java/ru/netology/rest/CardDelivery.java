3a2884d9b74a0c5f2e2d744b9282d84c856060c8
Nikolai Pustovoit <nikolaikop@gmail.com>
2021-12-27 10:40:13 -0700

    task2 new version

--- src/test/java/ru/netology/rest/CardDelivery.java
+++ src/test/java/ru/netology/rest/CardDelivery.java
@@ -2,20 +2,34 @@ package ru.netology.rest;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDelivery {
     int delay = 15;
-    int add7Days = 7;
+
+    @NotNull
+    private String when(boolean chose) {
+        Calendar cl = new GregorianCalendar();
+        cl.add(Calendar.DATE, 7);
+        if (chose) {
+            return new SimpleDateFormat("d").format(cl.getTime());
+        } else {
+            return new SimpleDateFormat("dd.MM.yyyy").format(cl.getTime());
+        }
+    }
 
     @BeforeEach
     void setUp() {
@@ -128,16 +142,13 @@ public class CardDelivery {
     void shouldSubmitTask2() {
         $("[data-test-id=city] .input__control").setValue("Ха");
         $$(".menu-item__control").findBy(text("Хабаровск")).click();
-        $(".icon-button__text>.icon_name_calendar").click();
-        $$(".calendar__day").findBy(text(LocalDate.now().plusDays(add7Days).format(DateTimeFormatter.ofPattern("d")))).click();
+        $("[data-test-id=date] [placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, when(false), Keys.CONTROL + "A", Keys.DELETE);
+        $$(".calendar__day").findBy(text(when(true))).click();
         $("[data-test-id=name] [name=name]").setValue("Николай Николаевич");
         $("[data-test-id=phone] [name=phone]").setValue("+71234567890");
         $("[data-test-id=agreement]>.checkbox__box").click();
         $("button>.button__content").click();
-        $("[data-test-id='notification'] .notification__title").shouldHave(Condition.text("Успешно!"),
-                Duration.ofSeconds(delay));
-        $("[data-test-id='notification'] .notification__content")
-                .shouldHave(Condition.text("Встреча успешно забронирована на " + LocalDate.now().plusDays(add7Days).format(DateTimeFormatter.ofPattern("d"))));
+        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(delay)).shouldHave(exactText("Успешно! Встреча успешно забронирована на " + when(false)));
     }
 
 }
